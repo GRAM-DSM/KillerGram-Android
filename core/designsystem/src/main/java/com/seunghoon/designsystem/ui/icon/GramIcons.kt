38e5f8d4e68c1e71736f306770ceb394ce36cf7f
Tmdhoon2 <hks026naver@gmail.com>
2024-04-22 10:09:35 +0900

    feat :: add svg icons

--- core/designsystem/src/main/java/com/seunghoon/designsystem/ui/icon/GramIcons.kt
+++ core/designsystem/src/main/java/com/seunghoon/designsystem/ui/icon/GramIcons.kt
@@ -11,4 +11,12 @@ object GramIcons {
     val WEIGHT_TRAINING = R.drawable.ic_weight_training
     val BASEBALL = R.drawable.ic_baseball
     val SOCCER = R.drawable.ic_soccer
+    val ADD = R.drawable.ic_add
+    val ARROW_DOWN = R.drawable.ic_arrow_down
+    val ARROW_LEFT = R.drawable.ic_arrow_left
+    val ARROW_RIGHT = R.drawable.ic_arrow_right
+    val ARROW_UP = R.drawable.ic_arrow_up
+    val CLOSE = R.drawable.ic_close
+    val OPTION = R.drawable.ic_option
+    val REFRESH = R.drawable.ic_refresh
 }
